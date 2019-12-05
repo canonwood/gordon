f34fdd6073d7125b6b9d65b3f57c3aec7a4e1e0f
Diego Lopez <aud0l@hotmail.com>
2019-12-04 23:34:10 -0600

    Agregar skeleton
    A partir de este punto voy a ir explicando los cambios de cada diff.
    
    No olviden que para ver los cambios que vamos realizando, deben ejecutar
    el comando `yarn start` en su terminal.
    
    1) public/index.html
    
    Agregamos la clase `bg-gray` a <body/>. Aún no se ve ningún por que la
    clase como tal no existe. En un momento la vamos a agregar.
    
    2) src/App.js
    
    Este componente será la vista principal de nuestra aplicación. Además
    pueden observar por primera vez como se utilizan las clases de
    tailwindcss aunque no tiene ningún efecto todavía.
    
    3) src/index.js
    
    Eliminamos el componente temporal que teníamos anteriormente en la línea
    6 y usamos el que acabamos de agregar. Además importamos las clases de
    estilos generadas por tailwindcss en la línea 4.
    
    En el navegador la aplicación se debe actualizar automáticamente y ahora
    si que pueden ver los estilos aplicados.
    
    4) src/tailwind.css
    
    ¿Recuerdas la clase que agregamos a la etiqueta <body/>? Pues en este
    archivo la vamos a definir. Deben colocar la clase tal como se muestra
    en el diff, de otro modo, puede que se genere algún error.
    
    ----
    
    Hasta este punto ya tenemos preparado para nuestra aplicación y ahora si
    nos vamos a enfocar por completo en la programación.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className="h-screen pt-20">
+        <div className="mx-auto max-w-xl">
+            <p className="text-center">
+                Gordon Messaging App
+            </p>
+        </div>
+    </main>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './vendor.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-
-const App = () => (<div>Gordon Messaging App</div>)
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

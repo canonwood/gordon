c5a52e0ded3cb375d3590c47ce95f2975fa4bb2a
Diego Lopez <aud0l@hotmail.com>
2019-12-04 23:21:04 -0600

    Remover archivos innecesarios
    Ahora vamos a remover archivos innecesarios que se generaron al crear el
    proyecto. Además vamos a agregar dos que nos van a ayudar a que nuestro
    código este formateado de manera consistente.
    
    1) Los archivos `.editorconfig` y `.prettierrc` los pueden ver en el
    diff de este commit. Para más información de cómo configurar las
    extensiones en sus editores, vean las siguientes direcciones:
    https://editorconfig.org/#download y https://prettier.io/docs/en/editors.html.
    
    Esta parte puede ser opcional pero vale la pena agregar esta
    configuración y tener un buen código desde el comienzo.
    
    2) Ahora vamos a eliminar los archivos de la siguiente lista:
    
    * src/App.css
    * src/App.js
    * src/index.css
    * src/logo.svg
    
    3) Y por último vamos a editar el archivo `src/index.js`. Por ahora
    vamos a mostrar un mensaje simple. Los cambios los puedes encontrar en
    el diff.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+const App = () => (<div>Gordon Messaging App</div>)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

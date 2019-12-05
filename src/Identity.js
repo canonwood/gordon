4d520df1e633eb0104e918ae913f19cfa62feef0
Diego Lopez <aud0l@hotmail.com>
2019-12-05 00:37:52 -0600

    Agregar form para login
    Ahora vamos a crear nuestro formulario para inicio de sesión.
    
    1) Ejecutamos el siguiente comando en nuestra terminal:
    
    $ yarn add classnames
    
    Classnames es una pequeña función que nos ayuda a agregar clases de
    manera dinámica y condicional a un componente. Por ejemplo: un botón
    puede contener la clase `btn`, y si tuvieramos una variable `isLogin`,
    entonces podríamos agregar otra adicional dependiendo de su valor
    verdadero o falso.
    
    2) src/Identity.js
    
    El diff nos muestra que debemos agregar y eliminar. [9] es una variable
    que nos ayuda a controlar el estado en el que se encuentra el
    formulario.  `working` se converite en verdadero cuando se está
    procesando la petición y es falso cuando esta en espera a ser rellenado
    o se muestra algún resultado.
    
    En [13] mostramos un diferente texto en el botón dependiendo del estado
    en el que se encuentre nuestro formulario. De igual manera hacemos con
    los estilos [15].
    
    La función en [20] es la que se va encargar de procesar nuestro
    formulario. Por ahora solo hacemos una pequeña simulación para que
    puedan observar el comportamiento. En [22] establecemos que el
    formulario se ha enviado y en [23] lo devolvemos a su estado original
    después de 4 segundos.
    
    ----
    
    Ahora puedes probar el formulario.

--- src/Identity.js
+++ src/Identity.js
@@ -1,20 +1,63 @@
-import React, { Component, useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import classnames from 'classnames';
 
 import { Context } from './Router';
 import Button from './com/Button.js';
 
 function Identity() {
   const { setRoute } = useContext(Context);
+  const [working, setWorking] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitText = working ? 'Working on...' : 'Sign In';
+
+  const submitClasses = classnames('mt-6 text-white', {
+    'bg-blue-600': !working,
+    'bg-blue-400': working,
+  });
+
+  function createOrLogin(e) {
+    e.preventDefault();
+    setWorking(true);
+    setTimeout(() => setWorking(false), 4000);
+  }
 
   return (
-    <div className="mx-auto max-w-lg bg-white border px-6 py-4">
-      <p>Identity App</p>
-      <Button
-        className="bg-blue-600 text-white"
-        onClick={() => setRoute('app')}
-      >
-        Sign In
-      </Button>
+    <div className="mx-auto max-w-sm bg-white border px-6 py-10">
+      <h1 className="text-2xl font-semibold text-center">Gordon</h1>
+
+      <p className="text-center text-gray-600">
+        Create or login into your account
+      </p>
+
+      <form className="mt-8" onSubmit={createOrLogin}>
+        <div>
+          <input
+            type="text"
+            placeholder="Your username"
+            className="px-2 py-1 w-full border rounded focus:border-blue-600"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            disabled={working}
+          />
+        </div>
+
+        <div className="mt-4">
+          <input
+            type="password"
+            placeholder="Your password"
+            className="px-2 py-1 w-full border rounded focus:border-blue-600"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={working}
+          />
+        </div>
+
+        <Button className={submitClasses} disabled={working}>
+          {submitText}
+        </Button>
+      </form>
     </div>
   );
 }

ee0ebaef167c46d5b78036863124a179f72ebfe7
Diego Lopez <aud0l@hotmail.com>
2019-12-05 00:51:32 -0600

    Agregar daemon
    Vamos a dejar por un momento la aplicación del cliente y ahora nos vamos
    a enfocar en el servidor.
    
    CRA por defecto tiene un servidor para que podamos desarrollar nuestra
    aplicación. Es el que hemos estado usando hasta ahora. Sin embargo, no
    lo podemos personalizar para incluir socket.io. Al menos, sin que
    recurramos al script `yarn eject`.
    
    Lo mejor será que vayamos creando nuestro propio servidor usando express
    y socket.io.
    
    1) .gitignore
    
    Es importante que no se olviden este cambio los que están usando git. De
    no hacerlo, van a agregar el directorio node_nodules del siguiente
    proyecto que vamos a incluir y no será para nada bueno.
    
    2) Creamos el directorio daemon y nos cambimos ahí con el siguiente
    comando:
    
    $ mkdir daemon; cd daemon
    
    3) Inicializamos un nuevo proyecto de node con el siguiente comando:
    
    $ yarn init
    
    Pueden ir agregando la información solicitada o presionar enter para
    agregar la respuesta predefinida. En el diff se pueden dar una idea de
    lo que yo he agregado.
    
    4) Ahora podemos agregar express a nuestro proyecto con el siguiente
    comando:
    
    $ yarn add express
    
    5) Agregamos el archivo daemon/router.js
    
    Este archivo contiene todas las rutas posibles que nuestro servidor
    puede responder. En esta parte solo agregamos la ruta index y le debe
    volver al navegador el mensaje `Gorgon Messaging App`.
    
    6) El último archivo que hay que agregar es daemon/index.js
    
    Contiene el código de nuestro servidor. Crea una aplicación express
    usando nuestro router que agregamos en el paso 5 y por defecto escucha
    en el puerto 4000. El 3000 es para nuestra aplicación CRA así que no lo
    podemos usar.
    
    ---
    
    A partir de este punto deben tener dos terminales abiertas al mismo
    tiempo. La primera para ejecutar nuestro servidor personalizado y la
    segunda para nuestra aplicación CRA. Ahora les muestro como iniciar el
    sevidor con el siguiente comando.
    
    $ node daemon/index.js
    
    Nota: El comando anterior sirve para cuando se encuentren dentro del
    directorio raíz de nuestro proyecto. El siguiente comando lo pueden usar
    cuando se encuentren dentro del directorio daemon:
    
    $ node index.js
    
    Ahora pueden abrir el navegador en la dirección `http://localhost:4000`
    y deben tener el mismo mensaje que el que declaramos en el archivo
    router.js.

---
+++ daemon/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+
+const router = require('./router');
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(router);
+
+const server = http.createServer(app);
+
+server.listen(PORT, function() {
+  console.log(`Gordon Daemon listening on port ${PORT}`);
+});
